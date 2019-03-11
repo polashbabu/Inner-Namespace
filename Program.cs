d868b825f9b8f2f95d012afffc524c9ce7bcc409
MD.Jahid Hassan <32840051+polashbabu@users.noreply.github.com>
2019-03-11 10:15:42 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using first;
+using first.second;
+
+
+
+namespace first
+{
+    class abc
+    {
+        public void function()
+        {
+            Console.WriteLine("first function ");
+
+
+        }
+    }
+    namespace second
+    {
+        class def
+        {
+            public void fuction()
+            {
+                Console.WriteLine("second function");
+
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            abc ob = new abc();
+            def obj = new def();
+            obj.fuction();
+            ob.function();
+            Console.ReadKey();
+
+
+        }
+    }
+}
+
